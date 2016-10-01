488d37f2b781938cc0ef2485f8c5389345f4e713
Matan Banner <matan.banner@gmail.com>
2016-10-01 10:31:07 +0300

    Add columns in user table for managing image by paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930212125) do
+ActiveRecord::Schema.define(version: 20160930224322) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,8 +20,12 @@ ActiveRecord::Schema.define(version: 20160930212125) do
     t.string   "profile_picture"
     t.string   "cover_photo"
     t.string   "status"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
