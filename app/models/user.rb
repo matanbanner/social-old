259f6b1b1cc74ab44eddd5d95ef90dc5c5d1db48
Matan Banner <matan.banner@gmail.com>
2016-10-01 19:32:39 +0300

    Create dummy users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,28 @@
 class User < ApplicationRecord
-  has_attached_file :image, styles: { medium: "160x160>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "160x160#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+
+
+  def self.populate_with_faker
+
+    image_filenames = Dir.glob(File.join(Rails.root, 'sampleimages', '*'))
+
+    users=[]
+
+    image_filenames.each do |filename|
+      name = Faker::Name.name
+      email = Faker::Internet.email(name)
+      country = Faker::Address.country
+      birthday = Faker::Date.between(100.years.ago, 16.years.ago)
+      file = open(filename)
+
+      users << User.new(name: name, email: email, country: country, birthday: birthday, image: file)
+
+    end
+
+    users.each{|user| user.save}
+
+
+  end
 end
