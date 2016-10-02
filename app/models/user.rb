68cb0ce5fcf539a0df190ef70fd3a6b072a56c9a
Matan Banner <matan.banner@gmail.com>
2016-10-02 11:58:26 +0300

    user associaltions
    user had many followings and followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,32 @@ class User < ApplicationRecord
   has_attached_file :image, styles: { medium: "160x160#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  has_many :user_follows, foreign_key: :from_user_id
+  has_many :followings, through: :user_follows, foreign_key: :from_user_id
+
+  has_many :user_followings, class_name: 'UserFollow', foreign_key: :to_user_id
+  has_many :followers, through: :user_followings, foreign_key: :to_user_id
+
+  def follow(user)
+    UserFollow.create(from_user_id: self.id, to_user_id: user.id)
+  end
+
+
+
+
+
+
+
+# make dummy records
+
+  def self.create_follows
+    User.all.each do |user|
+      followings = User.where.not(id: user.id).order("RANDOM()").take(rand(0..15))
+      followings.each do |following|
+        user.follow(following)
+      end
+    end
+  end
 
 
   def self.populate_with_faker
--- app/models/user_follow.rb
+++ app/models/user_follow.rb
@@ -1,2 +1,5 @@
 class UserFollow < ApplicationRecord
+  belongs_to :follower, class_name: 'User', foreign_key: :from_user_id
+  belongs_to :following, class_name: 'User', foreign_key: :to_user_id
+
 end
