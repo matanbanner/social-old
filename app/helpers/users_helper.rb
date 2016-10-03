fc4444745b6b9c7bdb1248ec4348acbb1845ed18
Matan Banner <matan.banner@gmail.com>
2016-10-03 19:31:14 +0300

    views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def user_name_link(user)
+    link_to user.name, user_url(user)
+  end
+
+  def user_image_link(user)
+    link_to image_tag(user.image(:thumb)), user_path(user)
+  end
+
+
+
+
+
 end
