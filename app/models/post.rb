35dc3dbc8f4351f275e2678e4e23fe1b3d1e86f8
Matan Banner <matan.banner@gmail.com>
2016-10-03 19:24:50 +0300

    Post can be NOT shared
    Until now shared post had to exist in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :publisher, class_name: 'User'
-  belongs_to :shared, class_name: 'Post'
+  belongs_to :shared, class_name: 'Post', optional: true # the post maybe a shared post of other post
 
 
 end
--- db/migrate/20161002165455_create_posts.rb
+++ db/migrate/20161002165455_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
       t.string :title
       t.text :body
       t.references :publisher, foreign_key: true
-      t.references :shared, foreign_key: true, default: nil
+      t.references :shared, foreign_key: true
 
       t.timestamps
     end
