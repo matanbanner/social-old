0965f9163db221b3eccbd47e7c9027bbc1e26fd7
Matan Banner <matan.banner@gmail.com>
2016-10-03 21:38:56 +0300

    New model - Comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.create(comment_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @post
+  end
+
+
+
+  private
+
+  def set_comment
+      @comment = Comment.new(comment_params)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :commenter_id)
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :publisher, class_name: 'User'
   belongs_to :shared, class_name: 'Post', optional: true # the post maybe a shared post of other post
+  has_many :comments
 
 
 end
